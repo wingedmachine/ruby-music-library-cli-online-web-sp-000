dab9604888b95050a6dca459a138f54d84170814
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-24 19:29:08 +0000

    automatically backed up by learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -2,13 +2,10 @@ class MusicImporter
   attr_accessor :path, :files
   def initialize(path)
     @path = path
-    @files = normalize_file_names(get_files_at(path))
+    @files = get_files_at(path)
   end
 
   def get_files_at(path)
-    Dir["#{path}/**.mp3"]
+    Dir["#{path}/**.mp3"].map(&:sub(path, ""))
   end
-
-  def normalize_file_names(files)
-    files.map { |file| file[0..-5] }
 end
