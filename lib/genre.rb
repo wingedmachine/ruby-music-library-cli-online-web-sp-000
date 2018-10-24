a3d04dd497aef2f998e6141070c0c00fbcb06857
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-24 19:43:56 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -15,23 +15,9 @@ class Genre
     @@all
   end
 
-  def self.destroy_all
-    @@all.clear
-  end
-
   def initialize(name)
     @name = name
     @songs = []
-  end
-
-  def save
-    @@all << self
-  end
-
-  def self.create(name)
-    genre = self.new(name)
-    genre.save
-    genre
   end
 
   def add_song(song)
