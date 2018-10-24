03097d236e3424e8a131ad11aaaf385e4e11c3ba
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-24 19:14:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,12 +40,4 @@ class Song
     song.save
     song
   end
-
-  def self.find_by_name(name)
-    @@all.detect { |song| song.name == name }
-  end
-
-  def self.find_or_create_by_name(name)
-    find_by_name(name) || create(name)
-  end
 end
