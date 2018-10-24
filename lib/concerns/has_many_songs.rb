42e3c5e70989a680c2c66dd4922d1e0b043bd594
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-24 20:17:41 +0000

    automatically backed up by learn

--- lib/concerns/has_many_songs.rb
+++ lib/concerns/has_many_songs.rb
@@ -3,7 +3,10 @@ module Concerns::HasManySongs
     @songs << song unless @songs.include?(song)
   end
 
-  def through_songs_has_many(objects)
+  def through_songs_has_many(objects, artist)
     @songs.map(&objects).uniq
+    wert = song.send(artist)
+    song.send("#{sender}=") unless wert
+    wert
   end
 end
