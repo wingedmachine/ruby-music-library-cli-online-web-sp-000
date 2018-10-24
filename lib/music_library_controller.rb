f2407516044eb004a5cdf360866a18971eb79048
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-24 20:48:56 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -13,6 +13,6 @@ class MusicLibraryController
     puts "To play a song, enter 'play song'."
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
-    gets.input
+    gets.strip
   end
 end
