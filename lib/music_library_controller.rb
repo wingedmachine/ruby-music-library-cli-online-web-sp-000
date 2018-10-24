6aca77b0d85f9a370c833f0727a514dcfd38493c
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-24 21:25:45 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -66,8 +66,11 @@ class MusicLibraryController
 
   def play_song
     puts "Which song number would you like to play?"
-    index = gets.strip.to_i - 1
-    song = @@song_list[index]
-    puts "Playing #{song.name} by #{song.artist.name}"
+    input = gets.strip
+    index = input.to_i - 1
+    if index
+      song = @@song_list[index]
+      puts "Playing #{song.name} by #{song.artist.name}"
+    end
   end
 end
