2f080c46cc8214f0181804e9323c1b22dbaefe4c
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-24 21:08:01 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -42,7 +42,7 @@ class MusicLibraryController
   end
 
   def list_songs_by_artist
-    
+
     # sort_by_name(Genre.all).each_with_index do |genre, i|
     #   puts "#{i+ 1}. #{genre.name}"
     # end
