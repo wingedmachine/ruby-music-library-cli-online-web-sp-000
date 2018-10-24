afb10c873180fd78f18c31e53a9ad2b7f3bdc3fc
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-24 20:05:32 +0000

    automatically backed up by learn

--- lib/concerns/has_many_songs.rb
+++ lib/concerns/has_many_songs.rb
@@ -1,7 +1,7 @@
 module Concerns::HasManySongs
   def add_song(song)
     @songs << song unless @songs.include?(song)
-    song.genre ||= self
+    song.send("genre") ||= self
   end
 
   def through_songs_has_many(objects)
